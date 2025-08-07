57cc2ccc2dc008c68f69f8f02fc41b69f0db9bb9
Nkhubalale <physimanuel@gmail.com>
2025-08-07 12:02:33 +0200

    initial setup

---
+++ Csharp-alx-control-flow/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_alx_control_flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
