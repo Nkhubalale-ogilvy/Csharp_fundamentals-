3251e2004156c6d344d049565575c3dbfe6bfc51
Nkhubalale <physimanuel@gmail.com>
2025-08-26 09:14:49 +0200

    Draft for reminder  logic

--- Csharp-alx-control-flow/Program.cs
+++ Csharp-alx-control-flow/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Csharp_alx_control_flow
+﻿Console.WriteLine("Enter your task: ");
+
+string? task = Console.ReadLine();
+
+Console.WriteLine($"You entered: {task}");
+
+Console.WriteLine("Enter Priority level as high/medium/low: ");
+
+string? priority = Console.ReadLine();
+
+Console.WriteLine("Is it time-bound? (yes/no): ");
+
+string? timeBound = Console.ReadLine();
+
+string reminder ="";
+
+if (priority != null)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    priority = priority.ToLower();
 }
+
+switch (priority)
+{
+    case "high":
+        reminder = $"Reminder: {task} is of high priority";
+        break;
+
+    case "medium":
+        reminder = $"Reminder: {task} is of medium priority";
+        break;
+
+    case "low":
+        reminder = $"Reminder: {task} is of low priority";
+        break;
+
+    default:
+        reminder = $"Your task {task} has an Invalid priority level entered.";
+        break;
+
+}
+
+if (timeBound == "yes")
+{
+
+    reminder += " that requires immediate attention today!";
+
+}
+
+Console.WriteLine(reminder);
