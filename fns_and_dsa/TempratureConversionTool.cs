a4469739328c6594114828fe9eb8cb8041c685d9
Nkhubalale <physimanuel@gmail.com>
2025-08-29 18:29:07 +0200

    Refactor code for  Temprature Tool  to free main

--- fns_and_dsa/TempratureConversionTool.cs
+++ fns_and_dsa/TempratureConversionTool.cs
@@ -1,5 +1,37 @@
 class TemperatureConversionTool
 {
+    public static void ConvertTemperature()
+    {
+        Console.WriteLine("Welcome to the Temperature Converter! \nPlease select the conversion type:");
+        Console.WriteLine("1. Celsius to Fahrenheit");
+        Console.WriteLine("2. Fahrenheit to Celsius");
+
+        string? conversionChoice = Console.ReadLine();
+        if (conversionChoice != "1" && conversionChoice != "2")
+        {
+            Console.WriteLine("Invalid choice. Exiting...");
+            return;
+        }
+
+        Console.Write("Enter the temperature to convert: ");
+        if (!double.TryParse(Console.ReadLine(), out double temperature))
+        {
+            Console.WriteLine("Invalid temperature. Exiting...");
+            return;
+        }
+
+        if (conversionChoice == "1")
+        {
+            string fahrenheit = CelsiusToFahrenheit(temperature);
+            Console.WriteLine($"{temperature}°C is {fahrenheit}");
+        }
+        else
+        {
+            string celsius = FahrenheitToCelsius(temperature);
+            Console.WriteLine($"{temperature}°F is {celsius}");
+        }
+    }
+
     public static string CelsiusToFahrenheit(double celsius)
     {
         try
