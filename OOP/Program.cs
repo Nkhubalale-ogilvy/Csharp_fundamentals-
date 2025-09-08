0880ba43bc44c3e89f51ba78f43fc2be11e50132
Nkhubalale <physimanuel@gmail.com>
2025-09-08 15:56:43 +0200

    polymophism demo calls implemented in main() progam.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Shape rect = new Rectangle(3, 3);
+        Shape circle = new Circle(10);
+
+        Console.WriteLine($"Rectangle area: {rect.Area()}");
+        Console.WriteLine($"Circle area: {circle.Area()}");
+
+        Shape[] shapes = new Shape[]
+        {
+            new Rectangle(3, 3),
+            new Circle(10),
+            
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"The area of the {shape.GetType()} is: {shape.Area()}");
+        }
+    }     
+}
