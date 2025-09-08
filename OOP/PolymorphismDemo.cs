e6c40e96e5856373e3e030b43162e4063ee79909
Nkhubalale <physimanuel@gmail.com>
2025-09-08 16:06:13 +0200

    Rundemo logic shifted from main to polymophism demo script

--- OOP/PolymorphismDemo.cs
+++ OOP/PolymorphismDemo.cs
@@ -39,3 +39,28 @@ class Circle : Shape
         return Math.PI * _radius * _radius;
     }
 }
+
+
+public class PolymorphismDemo
+{
+   public  static void RunDemo()
+    {
+        Shape rect = new Rectangle(3, 3);
+        Shape circle = new Circle(10);
+
+        Console.WriteLine($"Rectangle area: {rect.Area()}");
+        Console.WriteLine($"Circle area: {circle.Area()}");
+
+        Shape[] shapes = new Shape[]
+        {
+            new Rectangle(3, 3),
+            new Circle(10),
+            
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"The area of the {shape.GetType()} is: {shape.Area()}");
+        }
+    }     
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,22 +2,6 @@
 {
     static void Main()
     {
-        Shape rect = new Rectangle(3, 3);
-        Shape circle = new Circle(10);
-
-        Console.WriteLine($"Rectangle area: {rect.Area()}");
-        Console.WriteLine($"Circle area: {circle.Area()}");
-
-        Shape[] shapes = new Shape[]
-        {
-            new Rectangle(3, 3),
-            new Circle(10),
-            
-        };
-
-        foreach (var shape in shapes)
-        {
-            Console.WriteLine($"The area of the {shape.GetType()} is: {shape.Area()}");
-        }
+        PolymorphismDemo.RunDemo();
     }     
 }
