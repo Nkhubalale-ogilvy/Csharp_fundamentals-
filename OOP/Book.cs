0e10f7dd853c2ee08cea46bff3aaaf6b67650eaf
Nkhubalale <physimanuel@gmail.com>
2025-09-09 11:21:00 +0200

    Remove GC collecters

--- OOP/Book.cs
+++ OOP/Book.cs
@@ -34,9 +34,7 @@ public class RunBook
         Console.WriteLine(book);
 
         book = null;
-        GC.Collect(); // Forces immediate garbage collection 
-        GC.WaitForPendingFinalizers(); // Waits for the finalizer to complete ( destructor (~Book) must run)
-
+        
         Console.WriteLine(book);
         Console.WriteLine("The empty line above is because book is now null");
         
